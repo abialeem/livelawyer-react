f8ca45650b10abfd54e9b61ba98a02da38c41deb
abi aleem <aabidaleem143@gmail.com>
2024-12-23 17:51:41 +0530

    started currency exchanger to practice reusable components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import {useState,useEffect} from 'react';
-function App() {
-  const [count, setCount] = useState(0);
+import CurrencyExchanger from './components/CurrencyExchanger/CurrencyExchanger';
 
-  useEffect(()=>{
-        console.log(count);
-  },[count]);
-  
+function App() {
   return (
     <div className='w-full h-screen p-4 bg-black text-white' >
-      <h1 className='text-3xl bg-green-500 p-4 mb-2 ml-4 rounded-md'>Welcome to React!</h1>
-      <p>Count : {count}</p>
-      <button className='p-3 m-2' onClick={() => setCount(count + 1)} >Increase</button>
-      <button className='p-3 m-2' onClick={()=> setCount(count-1)}>Decrease</button>
-      <MainContent  name="aabid" />
-      <MainContent  name="isn't it" />
-      
+     
+      <CurrencyExchanger />
       
 
     </div>
@@ -22,12 +13,5 @@ function App() {
 }
 
 
-
-function MainContent(props){
-    
-  return(
-      <h3>React is great, {props.name}</h3>
-    );
-}
 
 export default App;
---
+++ src/components/CurrencyExchanger/CurrencyExchanger.jsx
@@ -0,0 +1,12 @@
+import {useState,useEffect} from 'react';
+
+function CurrencyExchanger(){
+
+    return(
+        <h2>Currency Exchanger</h2>
+        
+    )
+
+}
+
+export default CurrencyExchanger;
